08a0998d04d056c69d0ba4dbe2fc8d4e12597a2b
bayunaim <bayunaim07@gmail.com>
2023-03-08 10:39:57 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>CRUD - Laravel</title>
+        <title>CRUD - Laravel VueJS</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
